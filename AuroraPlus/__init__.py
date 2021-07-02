4f8abf9da6238e60495c2d03c61c178c516802b1
LeighCurran <leigh.curran@outlook.com>
2021-07-02 20:35:50 +1000

    Throw error if request times out

--- AuroraPlus/__init__.py
+++ AuroraPlus/__init__.py
@@ -27,41 +27,40 @@ class api:
         #Get access token
         try:
             token = self.session.post(self.url+'/identity/login',data={'username': username, 'password': password}, timeout=(2, 5))
+      
+            if (token.status_code == requests.codes.ok):
+
+                tokenjson = token.json()
+                self.token = tokenjson['accessToken']
+
+                #Request current customer data
+                current = self.session.get(self.url+'/customers/current',headers={'Authorization': self.token})
+                currentjson = current.json()[0]
+                customerId = currentjson['CustomerID']
+
+                #Loop through premises to get active premise
+                premises = currentjson['Premises']
+                for premise in premises:
+                    if (premise['ServiceAgreementStatus'] == 'Active'):
+
+                        #Get all service data here
+                        serviceAgreementID = premise['ServiceAgreementID']
+                        self.Active = premise['ServiceAgreementStatus'] 
+                        self.AmountOwed = premise['AmountOwed']
+                        self.EstimatedBalance = premise['EstimatedBalance']
+                        self.AverageDailyUsaged = premise['AverageDailyUsage']
+                        self.UsageDaysRemaining = premise['UsageDaysRemaining']
+                        self.AmountOwed = premise['AmountOwed']
+
+                        #Request data
+                        self.data = self.session.get(self.url + '/usage/' + timespan +'?serviceAgreementID=' + serviceAgreementID + '&customerId=' + customerId + '&index=-1', headers={'Authorization': self.token})
+                    
+                if (self.Active != 'Active'):
+                    self.Error = 'No active premise found'
+            else:
+                self.Error = token.reason
         except Timeout:
             self.Error = 'The request timed out'
-            #Fix up this
-
-        if (token.status_code == requests.codes.ok):
-
-            tokenjson = token.json()
-            self.token = tokenjson['accessToken']
-
-            #Request current customer data
-            current = self.session.get(self.url+'/customers/current',headers={'Authorization': self.token})
-            currentjson = current.json()[0]
-            customerId = currentjson['CustomerID']
-
-            #Loop through premises to get active premise
-            premises = currentjson['Premises']
-            for premise in premises:
-                if (premise['ServiceAgreementStatus'] == 'Active'):
-
-                    #Get all service data here
-                    serviceAgreementID = premise['ServiceAgreementID']
-                    self.Active = premise['ServiceAgreementStatus'] 
-                    self.AmountOwed = premise['AmountOwed']
-                    self.EstimatedBalance = premise['EstimatedBalance']
-                    self.AverageDailyUsaged = premise['AverageDailyUsage']
-                    self.UsageDaysRemaining = premise['UsageDaysRemaining']
-                    self.AmountOwed = premise['AmountOwed']
-
-                    #Request data
-                    self.data = self.session.get(self.url + '/usage/' + timespan +'?serviceAgreementID=' + serviceAgreementID + '&customerId=' + customerId + '&index=-1', headers={'Authorization': self.token})
-                
-            if (self.Active != 'Active'):
-                self.Error = 'No active premise found'
-        else:
-            self.Error = token.reason
 
 AuroraPlus = api("leigh.curran@outlook.com", "MuCEiD49%3Z%&y", "day")
 
