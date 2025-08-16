46d065b0db13af7d476453c3e921efb8cc377f8b
Olivier Mehani <shtrom@ssji.net>
2025-08-17 00:57:39 +1000

    repl: more token env variables

--- auroraplus/repl.py
+++ auroraplus/repl.py
@@ -1,6 +1,7 @@
 #!/bin/env python
 
 import code
+import json
 import logging
 import os
 import sys
@@ -11,19 +12,36 @@ from auroraplus.get_token import get_token
 log_level = logging.DEBUG
 logger = logging.getLogger(__name__)
 
+AURORAPLUS_ACCESS_TOKEN = "AURORAPLUS_ACCESS_TOKEN"
 AURORAPLUS_ID_TOKEN = "AURORAPLUS_ID_TOKEN"
+AURORAPLUS_TOKEN = "AURORAPLUS_TOKEN"
 
 
 def repl():
     logging.basicConfig(level=log_level)
-    if not (id_token := os.getenv(AURORAPLUS_ID_TOKEN)):
+
+    access_token = os.getenv(AURORAPLUS_ACCESS_TOKEN)
+    id_token = os.getenv(AURORAPLUS_ID_TOKEN)
+    token = None
+    if token_json := os.getenv(AURORAPLUS_TOKEN):
+        try:
+            token = json.loads(token_json)
+        except json.JSONDecodeError:
+            logger.warning(f"{AURORAPLUS_TOKEN} is not valid JSON")
+
+    if not any([token, id_token, access_token]):
         logger.info(
-            f"{AURORAPLUS_ID_TOKEN} environment variable unset or empty, requesting interactively ..."
+            f"All of {AURORAPLUS_TOKEN}, {AURORAPLUS_ID_TOKEN} and {AURORAPLUS_ACCESS_TOKEN} environment variables are empty, requesting interactively ..."
         )
         id_token = get_token()
 
-    api = auroraplus.AuroraPlusApi(id_token=id_token)
-    api.get_info()
+    try:
+        api = auroraplus.AuroraPlusApi(
+            token=token, id_token=id_token, access_token=access_token
+        )
+        api.get_info()
+    except Exception as exc:
+        logger.exception("exception when setting up", exc_info=exc)
     sys.ps1 = "AuroraPlus >>> "
     code.interact(
         local=locals(),
